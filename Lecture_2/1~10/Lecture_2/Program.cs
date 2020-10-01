a02d3c74f03bdf7e408dec23d22d83c5c1b4c215
minjun.ha <haminjun0@gmail.com>
2020-10-01 17:03:22 +0900

    2강 조건문 / 반복문 / 메서드 / 클래스

---
+++ Lecture_2/1~10/Lecture_2/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace Lecture_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // if, else, else if
+            {
+                bool test = false;
+                if (test)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+
+                int test1 = 2;
+                if (test1 == 1)
+                {
+                    // if
+                }
+                else if (test1 == 2)
+                {
+                    // else if
+                }
+                else
+                {
+                    // else
+                }
+            }
+
+            // switch
+            {
+                char character = 'c';
+                switch (character)
+                {
+                    case 'a':
+                        Console.WriteLine("a");
+                        break;
+                    case 'b':
+                    case 'c':
+                        Console.WriteLine("b or c");
+                        break;
+                    default:
+                        Console.WriteLine("default");
+                        break;
+                }
+            }
+
+            // while, do while
+            {
+                int count = 0;
+                while(count < 5)
+                {
+                    Console.WriteLine(count);
+                    count++;
+                }
+
+                do
+                {
+                    Console.WriteLine(count);
+                } while (count < 0);
+            }
+
+            // for, foreach
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(i);
+                }
+
+                int[] arr = new int[5] { 1, 2, 3, 4, 5 };
+                foreach (var item in arr)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            // break, continue
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i > 5)
+                        break;
+
+                    Console.WriteLine(i);
+                }
+
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i % 2 == 0)
+                        continue;
+
+                    Console.WriteLine(i);
+                }
+            }
+
+            // method
+            {
+                TestMethod();
+
+                // parameter funcation
+                int result = Plus(2, 5);
+                Console.WriteLine(result);
+            }
+
+            // ref, out
+            {
+                // ref
+                int test = 1;
+                RefFunc(ref test);
+                Console.WriteLine(test);
+
+                // out
+                if (Divide(2, 5, out int result))
+                {
+                    Console.WriteLine(result);
+                }
+            }
+        }
+
+        private static bool Divide(int num1, int num2, out int result)
+        {
+            if (num2 == 0)
+            {
+                result = 0;
+                return false;
+            }
+            result = num1 / num2;
+            return true;
+        }
+
+        private static void RefFunc(ref int test)
+        {
+            test++;
+        }
+
+        private static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static void TestMethod()
+        {
+            Console.WriteLine("TestMethod");
+        }
+    }
+}
---
+++ Lecture_2/Lecture_2/Lecture_2/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Lecture_2
+{
+    public class Cat
+    {
+        public string Name;
+        public string Color;
+
+        public Cat()
+        {
+            Name = "이름 없음";
+            Console.WriteLine("생성자1 호출!");
+        }
+
+        public Cat(string name) : this()
+        {
+            this.Name = name;
+            Console.WriteLine("생성자2 호출!");
+        }
+
+        public Cat(string name, string color) : this(name)
+        {
+            this.Color = color;
+            Console.WriteLine("생성자3 호출!");
+        }
+
+        ~Cat()
+        {
+            Console.WriteLine("소멸자 호출!");
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine("{0} : 야옹", Name);
+        }
+    }
+
+    public class StaticCat
+    {
+        public static string Name;
+        public static string Color;
+
+        public static void Meow()
+        {
+            Console.WriteLine("{0} : 야옹", Name);
+        }
+    }
+
+    class Program
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static int Add(int a, int b, int c, int d)
+        {
+            return a + b + c + d;
+        }
+
+        static int Add(params int[] array)
+        {
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                result += array[i];
+            }
+            return result;
+        }
+
+        static string DefaultParam(string test1, string test2 = "World")
+        {
+            return test1 + test2;
+        }
+
+        static void Main(string[] args)
+        {
+            // 함수 오버로딩
+            Console.WriteLine(Add(1, 2));
+            Console.WriteLine(Add(1, 2, 3));
+            Console.WriteLine(Add(1, 2, 3, 4));
+
+            // 가변 인자
+            Console.WriteLine(Add(1, 2, 3, 4, 5, 6));
+
+            // 디폴트 매개 변수
+            Console.WriteLine(DefaultParam("Hello"));
+            Console.WriteLine(DefaultParam("Hello", "C#"));
+
+            // 클래스의 생성
+            Cat kitty = new Cat();
+            kitty.Color = "하얀색";
+            kitty.Name = "키티";
+            kitty.Meow();
+            Console.WriteLine("{0} : {1}", kitty.Name, kitty.Color);
+
+            // 생성자 활용 
+            Cat nabi = new Cat("나비", "갈색");
+            nabi.Meow();
+            Console.WriteLine("{0} : {1}", nabi.Name, nabi.Color);
+
+            Cat copyCat = kitty;
+            kitty.Name = "진짜 키티";
+            copyCat.Name = "복사된 키티";
+
+            copyCat.Meow();
+            kitty.Meow();
+
+            StaticCat.Name = "모두의 고양이";
+            StaticCat.Color = "하얀색";
+            Console.WriteLine("{0} : {1}", StaticCat.Name, StaticCat.Color);
+
+            
+        }
+    }
+}
